18bc3a4c7ab025ff5884973e84400a2b475b90c4
sofijatodorovic <stodorovic9521rn@raf.rs>
2024-03-10 23:43:10 +0100

    Napravljena tabela za istoriju transakcija.

--- src/UserHomePage.tsx
+++ src/UserHomePage.tsx
@@ -1,6 +1,7 @@
 import AccountsTable from "./AccountsTable";
 import Header from "./Header";
-import { Account } from "./Model";
+import { Account, Transaction } from "./Model";
+import TransactionsTable from "./TransactionsTable";
 
 const UserHomePage = () => {
   const accounts: Account[] = [
@@ -8,10 +9,23 @@ const UserHomePage = () => {
     { number: "789012", balance: 500, availableBalance: 300 },
   ];
 
+  const transactions: Transaction[] = [
+    {
+      senderAccountNumber: "123456",
+      recipientName: "John Doe",
+      recipientAccountNumber: "789012",
+      amount: 200,
+      referenceNumber: "TRX123",
+      status: "Uspeh",
+      timestamp: "2024-03-08 14:30:00",
+    },
+  ];
+
   return (
     <div>
       <Header firstName={"Sofija"} lastName={"Todorovic"} />
       <AccountsTable accounts={accounts} />
+      <TransactionsTable transactions={transactions} />
     </div>
   );
 };
