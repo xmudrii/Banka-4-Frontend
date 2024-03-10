f2af3c3b3a1beba17891d1897ab4f1a0626437ba
sofijatodorovic <stodorovic9521rn@raf.rs>
2024-03-10 23:37:09 +0100

    Manje izmene.

--- src/UserHomePage.tsx
+++ src/UserHomePage.tsx
@@ -1,7 +1,17 @@
+import AccountsTable from "./AccountsTable";
+import Header from "./Header";
+import { Account } from "./Model";
+
 const UserHomePage = () => {
+  const accounts: Account[] = [
+    { number: "123456", balance: 1000, availableBalance: 800 },
+    { number: "789012", balance: 500, availableBalance: 300 },
+  ];
+
   return (
     <div>
-      <p>User hp</p>
+      <Header firstName={"Sofija"} lastName={"Todorovic"} />
+      <AccountsTable accounts={accounts} />
     </div>
   );
 };
