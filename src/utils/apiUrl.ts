d525b9008af021003e84b8cb04336e730ef833ce
Aleksa Buncic <33495263+CommandLineFox@users.noreply.github.com>
2024-05-28 21:16:58 +0200

    Env update (#182)
    * Env update
    
    * Na disablovo sam

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -15,11 +15,11 @@ export const getApiUrl = (route: string) => {
     ]
     const isKoisnikRoute = koisnikRoutes.some(prefix => route.startsWith(prefix));
     if (isKoisnikRoute) {
-        return 'https://banka-4-dev.si.raf.edu.rs/user-service/api'
+        return process.env.REACT_APP_USER_URL
     }
     const isBankaRoute = bankaRoutes.some(prefix => route.startsWith(prefix));
     if (isBankaRoute) {
-        return 'https://banka-4-dev.si.raf.edu.rs/banka-service/api'
+        return process.env.REACT_APP_BANKA_URL
     }
     return 'https://banka-4-dev.si.raf.edu.rs/berza-service/api'
 }
